48986701b795370b413bf10aa967c9a6dca945a4
Nathan Fries <npfries@gmail.com>
2023-09-11 12:55:56 -0400

    placeholder prisma client for tests

---
+++ apps/auth-service/generated/client/index.js
@@ -0,0 +1,5 @@
+// this file is a placeholder that get overwritten
+// this is a workaround to allow Jest to run before Prisma has generated the client
+
+class PrismaClient {}
+export { PrismaClient };
