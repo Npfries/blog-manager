aa299711a662c9a76c5336ee353047341c64b1b3
Nathan Fries <npfries@gmail.com>
2023-09-11 13:03:26 -0400

    pretest hook

--- apps/auth-service/generated/client/index.js
+++
@@ -1,5 +0,0 @@
-// this file is a placeholder that get overwritten
-// this is a workaround to allow Jest to run before Prisma has generated the client
-
-class PrismaClient {}
-export { PrismaClient };
